948fcf1e181f406d66136fef093078621a82cb48
pabbathi rakesh <rakeshpabbathi15@gmail.com>
2024-02-27 00:43:35 +0530

    math module

---
+++ Miscellaneous Topics/stdlib.py
@@ -0,0 +1,5 @@
+# python standard libray
+# Working with some python  python standard libray are math module,random module,choice module,map,filter,reduce
+import math 
+print(math.factorial(5))
+print(math.pi)
